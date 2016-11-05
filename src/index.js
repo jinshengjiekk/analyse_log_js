2299d74a5f2cd136e477b461def9f5011e20fb5b
jinshengjie(Mac) <jinshengjiekk@163.com>
2016-11-05 16:27:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,33 +1,127 @@
-/**
- * Created by JINSHENGJIE on 16/11/01 .
- */
-
-var content;
-
+let content;
+let keysArr = [];
+let contentArr = [];
+let lastArr = [];
 function analysis(ele) {
-	var files = ele.target.files;
-	var selectedFile = files[0];
-	var name = selectedFile.name;//读取选中文件的文件名
-	var size = selectedFile.size;//读取选中文件的大小
+	let files = ele.target.files;
+	let selectedFile = files[0];
+	let name = selectedFile.name;
+	let size = Math.round(selectedFile.size/1024);
 	console.log("文件名:" + name + "大小：" + size);
 
-	var reader = new FileReader();//这里是核心！！！读取操作就是由它完成的。
+	let reader = new FileReader();//这里是核心
 	reader.readAsText(selectedFile);//读取文件的内容
 
 	reader.onload = function () {
 		content = this.result;//当读取完成之后会回调这个函数，然后此时文件的内容存储到了result中。
-		console.info(content);
-		document.getElementById('filtered-content').innerHTML = content;
+		contentArr = content.split(/[\n|\r\n]{2,}/);
+		document.getElementById('file-info').innerHTML = `<div><span>文件名称：${name}</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>文件大小:${size}KB</span></div>`;
+		document.getElementById('filtered-title').innerHTML = '原始内容：';
+		document.getElementById('filtered-content').innerHTML = content.replace(/[\r|\r\n]{2,}/g, '<br/><hr/>');
 	}
 }
 
 
-/*
+$(document).ready(function () {
 
-$(document).ready(function(){
-	$('.file-title').css({'background-color': 'red'});
+	//点击确定开始搜索
+	$('#start-search').click(function () {
+		let showKey;
+		if (!content) {
+			alert('请选择要分析的java堆栈文本文件！');
+			return;
+		}
+		let key = $('#keys-input').val().trim();
+		showKey = key;
+		if (!key) {
+			alert('关键字不能为空！');
+			return;
+		}
 
-	$('#isOk').change(function () {
-		$('#filtered-content').innerHTML = content;
+		if (key.split(/\s+/).length > 2) {
+			alert(`关键字形式为:
+						1: keyword(包含）
+						2: -v keyword 或者 keyword -v(取反，不包含)`);
+			return;
+		}
+		//没有直接replace是考虑到关键字中可能包含'-v'
+		if (key.startsWith('-v') || key.endsWith('-v')) {
+			key = key.replace('-v', '').trim() + '&&&*** ';
+		} else {
+			if (key.split(/\s+/).length > 1) {
+				alert(`关键字形式为:
+						1: keyword(包含）
+						2: -v keyword 或者 keyword -v(取反，不包含)`);
+				return;
+			}
+		}
+
+
+		if (keysArr.includes(key)) {
+			alert('关键字已存在！');
+			return;
+		} else {
+			keysArr.push(key);
+			$('#list-key').append(`<input type="checkbox" name= "key" value=${key} checked><span>${showKey}</span>`).click();
+		}
+
+		$('#keys-input').val('');
 	});
-});*/
+
+	//利用冒泡机制监听动态生成的复选框上层div的click事件
+	$('#list-key').click(function () {
+		let arr = [];
+		$(':checked').each(function () {
+			arr.push(this.value);
+		});
+		if (arr.length == lastArr.length) {
+			return;
+		}
+		lastArr = arr;
+		filter(arr);
+	});
+
+	//过滤方法
+	function filter(arr) {
+		let regexpArr = [];
+		let output = '';
+		let result = '';
+		for (let i of arr) {
+			if (i.endsWith('&&&***')) {
+				regexpArr.push(new RegExp('^((?!' + i.replace('&&&***', '') + ').)*$','gi'));
+			} else {
+				regexpArr.push(new RegExp(i,'gi'));
+			}
+		}
+
+		let filteredContentArr = contentArr.filter(function (data) {
+			return processMatch(data, regexpArr);
+		});
+
+		for (let ele of filteredContentArr) {
+			output += '<p>' + ele + '</p><hr>';
+		}
+		$('#filtered-title').html('');
+		result = `<p>过滤后满足条件的线程数：${filteredContentArr.length}</p>` + output;
+		$('#filtered-content').html(result);
+	}
+
+	function processMatch(data, regexpArr) {
+		for (let i of regexpArr) {
+			if (!i.test(data.replace(/[\r|\r\n]/g, ''))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//监听键盘回车键，默认按下就是确定搜索
+	$(document).keydown(function (event) {
+		if (event.keyCode == 13) {
+			console.info('sdf');
+			$('#start-search').click();
+		}
+
+
+	});
+});
